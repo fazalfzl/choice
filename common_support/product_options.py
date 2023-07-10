a361dba399a8cd4d652528d0439b0f377f929fc7
fazalfzl <mhdfazalvs@gmail.com>
2023-07-10 13:34:30 +0530

    first

--- common_support/product_options.py
+++ common_support/product_options.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QWidget, QLabel, QPushButton
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
 
 from common_support.ui.product_options import Ui_Form
 from common_support.utils import filter_list, get_product_name_list, Config, get_product_details_by_name
@@ -21,20 +22,30 @@ class UI_product_options(QWidget, Ui_Form):
         products= filter_list(strings=self.product_name_list,search_text=label)
 
         for product in products:
+            container = QWidget(self)
+            layout = QVBoxLayout(container)
             btn:QPushButton=QPushButton(self)
             btn.setFixedSize(100,100)
-            product_details_by_name = get_product_details_by_name(product)
-            price=product_details_by_name['price']
-            btn.setText(product+price)
-            btn.setStyleSheet(f"border-image: url('{self.ai_products_dpath}/{product}.gif');")
-            self.SAWC.layout().addWidget(btn)
+            btn.product=product
             btn.clicked.connect(self.product_clicked)
+            btn.setStyleSheet(f"border-image: url('{self.ai_products_dpath}/{product}.gif');")
+            label = QLabel(container)
+            product_details_by_name = get_product_details_by_name(product)
+            price = product_details_by_name['price']
+            label.setText(f"{product} - {price}")
+            label.setAlignment(Qt.AlignCenter)
+            layout.addWidget(btn)
+            layout.addWidget(label)
+            self.SAWC.layout().addWidget(container)
 
 
     def product_clicked(self):
         btn: QPushButton = self.sender()
-        if self.add_product_to_current_table :
-            self.add_product_to_current_table(btn.text())
+        try:
+            if self.add_product_to_current_table :
+                self.add_product_to_current_table(btn.product)
+        except Exception as e:
+            print(e)
 
 
 
